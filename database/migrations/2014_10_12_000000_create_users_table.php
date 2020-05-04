ce7ab107231f19014df1bf489faf720c81ee706a
imad730 <64793563+imad730@users.noreply.github.com>
2020-05-04 16:18:48 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,7 +8,7 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * hahahahahahahahahahhahahahahaha
      * @return void
      */
     public function up()
